6089421f97db7e462a78d8d3ca1a9d32fc093fba
dahooz <dahooz@online.de>
2018-07-06 00:21:50 +0200

    Supply additional command-line arguments to pytest
    Additional arguments can be supplied via ROS launchfiles using the `args` attribute.

--- src/ros_pytest/__init__.py
+++ src/ros_pytest/__init__.py
@@ -12,10 +12,18 @@ def get_output_file(argv):
 
     raise RuntimeError('No output file has been passed')
 
+def get_add_args(argv):
+    # don't include the --gtest, as that is the first non-usersupplied arg
+    end = next(argv.index(arg) for arg in argv if arg.startswith('--gtest_output'))
+    # strip executable path as well
+    return argv[1:end]
 
 def run_pytest(argv):
     output_file = get_output_file(argv)
     test_module = rospy.get_param('test_module')
     module_path = os.path.realpath(test_module)
+    add_args = get_add_args(argv)
 
-    return pytest.main([module_path, '--junitxml={}'.format(output_file)])
+    call_args = [module_path, '--junitxml={}'.format(output_file)]
+    call_args.extend(add_args)
+    return pytest.main(call_args)
